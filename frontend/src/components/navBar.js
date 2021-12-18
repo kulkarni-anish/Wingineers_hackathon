eac7234fdf84c72f6cbf30afb46a0f6577b8fabd
nevilbhimani <bhimaninevil72@gmail.com>
2021-12-19 01:25:33 +0530

    login component added with styling and responsiveness ,formik and yup is used for forms and validation

---
+++ frontend/src/components/navBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/navBar.scss";
+import Typography from "@mui/material/Typography";
+const NavBar = () => {
+  return (
+    <div className="navBar">
+      <Typography className="navBar-header">alibaba</Typography>
+    </div>
+  );
+};
+
+export default NavBar;
