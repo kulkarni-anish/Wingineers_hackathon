7012375695485825aa6a9febb0001039870f57b9
hetvi <hetvishah1801@gmail.com>
2021-12-19 16:56:04 +0530

    profile page styling

--- frontend/src/pages/manufacturerProfile.js
+++ frontend/src/pages/manufacturerProfile.js
@@ -6,22 +6,27 @@ import BasicTable from "../components/aboutManufacturer";
 import NavBar from "../components/navBar";
 import "../styles/manufacturerProfile.scss";
 import profile_img from "../assets/profile_img..jpeg"
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import StarRateIcon from '@material-ui/icons/StarRate';
 function Profile(){
     return(
         <div className="manufacturerProfile">
             <NavBar />
             <Grid item xs={12} className="manufacturerProfile-profile">
-                My Profile
+                < AccountCircleIcon className="manufacturerProfile-icon"/>My Profile
             </Grid>
+            
             <Grid container >
                 <Grid item xs={12} sm={5}>
                     <img src={profile_img} className="manufacturerProfile-image"></img>
                     <Typography className="manufacturerProfile-text">Review:</Typography>
                 </Grid>
                 <Grid xs={12} sm={7}>
-                    <Typography className="manufacturerProfile-text-1">CompanyName</Typography>
-                    <Typography className="manufacturerProfile-text ">Buisness Type</Typography>
-                    <Typography className="manufacturerProfile-text ">Address</Typography>
+                    <Typography className="manufacturerProfile-text1">ManuName</Typography>
+                    <Typography className="manufacturerProfile-text2">Buisness Type</Typography>
+                    <Typography className="manufacturerProfile-text3">Address</Typography>
+                    <Typography className="manufacturerProfile-text4">Rating<StarRateIcon/><StarRateIcon/><StarRateIcon/><StarRateIcon/><StarRateIcon/></Typography>
+                    
                     <BasicTable />
                 </Grid>
             </Grid>
