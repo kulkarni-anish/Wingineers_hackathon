a8b717d6578840efd374abee7f0c6e22089b7483
nevilbhimani <bhimaninevil72@gmail.com>
2021-12-20 15:10:22 +0530

    prduct page routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/home";
 import Company from "./components/companyform";
 import Profile from "./pages/manufacturerProfile";
 import Manufacturer from "./components/manufactureform";
+import Product from "./components/Product";
 import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/home" element={<Home />} />
+          <Route path="/home/product" element={<Product />} />
           <Route path="/login/*" element={<Login />} />
           <Route path="/Profile/*" element={<Profile />} />
         </Routes>
---
+++ frontend/src/pages/Product.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "../styles/product.scss";
+const Product = () => {
+  return <div>product page</div>;
+};
+
+export default Product;
